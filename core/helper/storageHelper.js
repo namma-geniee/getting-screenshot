c2818b081a9b98cf8cbac477d27b3f754561e28e
Mai Anh Nam <namma@geniee.co.jp>
2017-09-04 10:33:24 +0700

    Change storage folder structure

--- core/helper/storageHelper.js
+++ core/helper/storageHelper.js
@@ -4,6 +4,7 @@ const path = require('path')
 const moment = require('moment')
 
 const STORAGE_ROOT_PATH = path.join(__dirname, '../../storage')
+const STORAGE_DATA_PATH = path.join(STORAGE_ROOT_PATH, 'data')
 
 /**
  * @param {string} storageRelativePath
@@ -18,7 +19,7 @@ exports.getStoragePath = function (storageRelativePath ) {
  * @returns {string}
  */
 exports.getAdnetworkStoragePath = function (adnetworkName) {
-  return this.getStoragePath(adnetworkName)
+  return path.join(STORAGE_DATA_PATH, adnetworkName)
 }
 
 /**
@@ -27,7 +28,9 @@ exports.getAdnetworkStoragePath = function (adnetworkName) {
  * @param {Date} date
  * @returns {string}
  */
-exports.getAccountStoragePath = function (adnetworkName, accountName, date = undefined) {
+exports.getAccountStoragePath = function (adnetworkName, accountName, date) {
   const yearAndMonth = date ? moment(date).format('YYYY-MM') : undefined
-  return this.getStoragePath(`${adnetworkName}/${accountName}/${yearAndMonth}`)
+  const adnetworkPath = this.getAdnetworkStoragePath(adnetworkName)
+
+  return path.join(adnetworkPath, `${yearAndMonth}/${accountName}`)
 }
